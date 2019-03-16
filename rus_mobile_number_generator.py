b309a897d7eea36caf4817219f4eabd8b6acdffa
marsel <mshagiev@inbox.ru>
2019-03-16 14:40:01 +0300

    Generate phone number

--- rus_mobile_number_generator.py
+++ rus_mobile_number_generator.py
@@ -1,8 +1,6 @@
 
-
-def parse_operator_codes_file(input_file='indexmain.ru_collected.txt'):
-    digits = [fetch_digits(line) for line in open(input_file, "r").readlines() if is_oper_code_line(line)]
-    print digits
+def parse_operator_codes_file(input_file='indexmain.ru_collected_trunked.txt'):
+    return [fetch_digits(line) for line in open(input_file, "r").readlines() if is_oper_code_line(line)]
 
 
 def is_oper_code_line(input_line):
@@ -14,16 +12,26 @@ def is_oper_code_line(input_line):
 
 #u'+7 916 99x-xx-xx\t0000000..9999999\t1' to u'91699'
 def fetch_digits(input_line):
-    print input_line
     region = [char for char in input_line.split('\t')[0].split(' ')[-1] if char.isdigit()]
     return ''.join(input_line.split(' ')[1] + ''.join(region))
 
-
-def main():
-    parse_operator_codes_file()
+# 7 - country_code
+# 915 - operator_code
+# 6783452 - postfix
+def main(dict_file):
+    country_codes = [7, 8]
+    for operator_code in parse_operator_codes_file():
+        digits_left = 11 - operator_code.__len__() - 1 #minus one digit for country_code
+        for postfix in range(0, 10**digits_left - 1):
+            for country_code in country_codes:
+                phone_number = str(country_code) + str(operator_code) + str(postfix).zfill(digits_left) + '\n'
+                dict_file.write(phone_number)
+                # print phone_number
 
 
 if __name__ == '__main__':
-    main()
+    dictionary = open("phone_num_dict.txt", 'w')
+    main(dictionary)
+    dictionary.close()
 
 
